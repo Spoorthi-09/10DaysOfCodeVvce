801374f6c3a9f0958394486f96b3d7d2d42d5e45
Spoorthi-09 <55314359+Spoorthi-09@users.noreply.github.com>
2020-08-21 17:34:55 +0530

    day 4

--- queueusing2stacks.java
+++ queueusing2stacks.java
@@ -8,50 +8,47 @@ public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner sc = new Scanner(System.in);
-        int max = 10000010;
-        int front = -1;
-        int rear = -1;
-        int count = 0;
-        int[] arr = new int[max];
+   Scanner sc = new Scanner(System.in);
+   int max = 1000010;
+   int[] frontarr = new int[max];
+   int[] reararr = new int[max];
 
-        int query = sc.nextInt();
+   int front = -1;
+   int rear = -1;
+   int query = sc.nextInt();
 
-        for(int i=0;i<query;i++)
+   for(int i=0;i<query;i++)
+   {
+       int choice = sc.nextInt();
+
+       if(choice == 1)
+       {
+           int number = sc.nextInt();
+            rear = rear+1;
+            reararr[rear] = number;       
+        }else 
         {
-            int choice = sc.nextInt();
-
-            if(choice == 1)
+            if(front == -1)
             {
-                if (front == -1) 
-                front = 0;
-
-                int number = sc.nextInt();
-                rear = rear + 1;
-                arr[rear] = number;
-                count++;
-            }else if(choice == 2)
-            {
-                if(count==0)
+                while(rear != -1)
                 {
-                    System.out.println("Underflow");
-                    i++;
-                }else{
-                front = (front+1);
-                count--; 
+                    front = front+1;
+                    frontarr[front] = reararr[rear];
+                    rear = rear - 1;
                 }
-            }else if(choice == 3)
-            {
-                if(count==0){
-                    System.out.println("Underflow");
-                }
-                System.out.println(arr[front]);
             }
-            
-            
-            
-            
+            if(front != -1)
+            {
+                if(choice == 2)
+                {
+                    front = front-1;
+                }else if(choice == 3)
+                {
+                    System.out.println(frontarr[front]);
+                }
+            }
         }
-        
+
     }
+}
 }
